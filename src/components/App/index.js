db2543c3897ab84411a3a6f9bad6966c51f7984f
shalahudinfadil <fadilbhakti@gmail.com>
2020-08-23 02:39:11 +0700

    First Commit. TODO: Fix AdminLTE 3 treeview Error

---
+++ src/components/App/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Navbar from '../Navbar';
+import MainSidebar from '../MainSidebar';
+import Content from '../Content';
+import Footer from '../Footer';
+
+const App = () => (
+    <>
+        <Navbar />
+        <MainSidebar />
+        <Content />
+        <Footer />
+    </>
+);
+
+export default App;
